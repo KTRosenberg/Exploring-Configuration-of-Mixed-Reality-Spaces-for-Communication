b89de0ff8fd2de0482ac2a99629f33595865d2d2
KTRosenberg <ktr254@nyu.edu>
2019-02-22 18:37:02 -0500

    made init up-to-date with master

--- Assets/Scripts/utility/Init.cs
+++ Assets/Scripts/utility/Init.cs
@@ -6,10 +6,12 @@ using System.IO;
 using System.Text;
 
 
-public class Init : MonoBehaviour {
+public class Init : MonoBehaviour
+{
 
     // init glow
     public GameObject glowPrefab;
+    public string globalConfigName;
 
     //init teleportation for Camera Rig
     public GameObject cameraRig;
@@ -17,47 +19,30 @@ public class Init : MonoBehaviour {
 
     void Awake()
     {
-        
+
     }
 
-    public bool createDefaultConfigFileIfDoesNotExist = false;
-    void GenerateDefaultConfigFile()
-    {
-        FileStream stream;
-        if (!File.Exists("GlobalConfig.xml")) {
-            byte[] defaultGlobalConfigXML = GlobalToggleIns.GetDefaultConfigXMLBytes();
 
-            stream = new FileStream("GlobalConfig.xml", FileMode.CreateNew);
-
-            stream.Write(defaultGlobalConfigXML, 0, defaultGlobalConfigXML.Length);
-            stream.Flush();
-            stream.Close();
-
-            Debug.Log("<color=green>wrote default GlobalConfig.xml</color>");
-        }
-    }
     void Start()
     {
         var serializer = new XmlSerializer(typeof(Xml2CSharp.GlobalToggle));
-
-        if(createDefaultConfigFileIfDoesNotExist) {
-            GenerateDefaultConfigFile();
-        }
-        FileStream stream = null;
-        if (File.Exists("GlobalConfig.xml")) {
-            stream = new FileStream(("GlobalConfig.xml"), FileMode.Open);
+        var stream = new FileStream(("GlobalConfig.xml"), FileMode.Open);
+        if (stream != null) {
+            Debug.Log("<color=green>load GlobalConfig.xml</color>");
             var container = serializer.Deserialize(stream) as Xml2CSharp.GlobalToggle;
             GlobalToggleIns.GetInstance().MRConfig = Utility.StringToConfig(container.MRConfig);
             GlobalToggleIns.GetInstance().username = container.username;
             stream.Close();
             print("change to config:" + GlobalToggleIns.GetInstance().MRConfig);
             GlobalToggleIns.GetInstance().assignToInspector();
-
+        }
+        else {
+            Debug.Log("<color=red>GlobalConfig.xml not found, use inspector value directly.</color>");
+            Debug.Log("<color=red>SampleGlobalConfig.xml is the example file for you to create GlobalConfig.xml. Create one and put it into root folder.</color>");
         }
 
-        Instantiate(glowPrefab); //create the glow outline
+        Instantiate(glowPrefab);
         SetUpTeleportation();
-
     }
 
     //this void attaches the teleportation script to the camera
