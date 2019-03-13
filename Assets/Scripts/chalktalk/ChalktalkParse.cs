e05f2b83e0c6bca96bfb57ee3007628731d8865e
KTRosenberg <ktr254@nyu.edu>
2019-03-12 21:35:33 -0400

    Temporarily made text visible, but runtime errors persist
    The error does not stop the drawing of text if each stroke drawn in a sub-function call, but the error still exists. Stack-trace:
    
    transform.localPosition assign attempt for 'T:24:6' is not valid. Input localPosition is { NaN, NaN, 0.000000 }.
    UnityEngine.Transform:set_localPosition(Vector3)
    Chalktalk.SketchCurve:DrawTextMeshText() (at Assets/Scripts/chalktalk/SketchCurve.cs:107)
    Chalktalk.SketchCurve:ApplyTransform(List`1) (at Assets/Scripts/chalktalk/SketchCurve.cs:212)
    Chalktalk.CTEntityPool:ApplyBoard(List`1) (at Assets/Scripts/chalktalk/CTEntityPool.cs:341)
    Chalktalk.Renderer:Update() (at Assets/Scripts/chalktalk/Renderer.cs:109)

--- Assets/Scripts/chalktalk/ChalktalkParse.cs
+++ Assets/Scripts/chalktalk/ChalktalkParse.cs
@@ -37,114 +37,112 @@ namespace Chalktalk
 
         void ParseEachStroke(byte[] bytes, ref int cursor, ref List<SketchCurve> sketchLines, ref CTEntityPool pool)
         {
-            for (; cursor < bytes.Length;) {
-                //The length of the current line
-                int length = Utility.ParsetoInt16(bytes, cursor);
-                cursor += 2;
-                // if the line data is less than 12, we skip this one curve
-                if (length < 12)
-                    return;
+            //The length of the current line
+            int length = Utility.ParsetoInt16(bytes, cursor);
+            cursor += 2;
+            // if the line data is less than 12, we skip this one curve
+            if (length < 12)
+                return;
 
-                // The ID of current line, TODO: could be assigned as sketchPage index
-                int ID = Utility.ParsetoInt16(bytes, cursor);
-                //TODO
-                //bool norender = false;
-                //if (GlobalToggleIns.GetInstance().MRConfig == GlobalToggle.Configuration.eyesfree && ID > 0)
-                //norender = true;
+            // The ID of current line, TODO: could be assigned as sketchPage index
+            int ID = Utility.ParsetoInt16(bytes, cursor);
+            //TODO
+            //bool norender = false;
+            //if (GlobalToggleIns.GetInstance().MRConfig == GlobalToggle.Configuration.eyesfree && ID > 0)
+            //norender = true;
 
-                //ID = 0;
-                cursor += 2;
+            //ID = 0;
+            cursor += 2;
 
-                // Parse the color of the line
-                Color color = Utility.ParsetoColor(bytes, cursor);
-                cursor += 4;
+            // Parse the color of the line
+            Color color = Utility.ParsetoColor(bytes, cursor);
+            cursor += 4;
 
-                //Parse the Transform of this Curve; new version, use real float instead of fake float
-                Vector3 translation = Utility.ParsetoRealVector3(bytes, cursor, 1);
-                cursor += 6 * 2;
-                Quaternion rotation = Utility.ParsetoRealQuaternion(bytes, cursor, 1);
-                cursor += 6 * 2;
-                float scale = Utility.ParsetoRealFloat(bytes, cursor);
-                cursor += 2 * 2;
-                //Debug.Log("header transformation:" + translation.ToString("F3") +"\t"+ scale.ToString("F3"));
+            //Parse the Transform of this Curve; new version, use real float instead of fake float
+            Vector3 translation = Utility.ParsetoRealVector3(bytes, cursor, 1);
+            cursor += 6 * 2;
+            Quaternion rotation = Utility.ParsetoRealQuaternion(bytes, cursor, 1);
+            cursor += 6 * 2;
+            float scale = Utility.ParsetoRealFloat(bytes, cursor);
+            cursor += 2 * 2;
+            //Debug.Log("header transformation:" + translation.ToString("F3") +"\t"+ scale.ToString("F3"));
 
-                //Parse the type(line, filled, text) of the stroke
-                int type = Utility.ParsetoInt16(bytes, cursor);
-                cursor += 2;
-                //Debug.Log("CT type:" + type);
-                //Parse the width of the line
-                float width = 0;
-                //Debug.Log("Current Line's points count: " + (length - 12) / 4);
+            //Parse the type(line, filled, text) of the stroke
+            int type = Utility.ParsetoInt16(bytes, cursor);
+            cursor += 2;
+            //Debug.Log("CT type:" + type);
+            //Parse the width of the line
+            float width = 0;
+            //Debug.Log("Current Line's points count: " + (length - 12) / 4);
 
-                if (GlobalToggleIns.GetInstance().poolForSketch == GlobalToggle.PoolOption.NotPooled) {
+            if (GlobalToggleIns.GetInstance().poolForSketch == GlobalToggle.PoolOption.NotPooled) {
 
-                }
-                else {
-                    ChalktalkDrawType ctType = (ChalktalkDrawType)type;
-                    // parse text
-                    if (ctType == ChalktalkDrawType.TEXT) {
-                        string textStr = ParseTextForEachStroke(bytes, ref cursor, length);
-                        if (textStr.Length >= 0) {
-                            SketchCurve curve = pool.GetCTEntityText();
+            }
+            else {
+                ChalktalkDrawType ctType = (ChalktalkDrawType)type;
+                // parse text
+                if (ctType == ChalktalkDrawType.TEXT) {
+                    string textStr = ParseTextForEachStroke(bytes, ref cursor, length);
+                    if (textStr.Length > 0) {
+                        SketchCurve curve = pool.GetCTEntityText();
+                        curve.InitWithText(textStr, translation, scale, 0/*renderer.facingDirection*/, color, ctType, ID);
+                        sketchLines.Add(curve);
+                        if (GlobalToggleIns.GetInstance().MRConfig == GlobalToggle.Configuration.eyesfree && ID == ChalktalkBoard.activeBoardID) {
+                            curve = pool.GetCTEntityText();
                             curve.InitWithText(textStr, translation, scale, 0/*renderer.facingDirection*/, color, ctType, ID);
+                            curve.isDup = true;
                             sketchLines.Add(curve);
-                            if (GlobalToggleIns.GetInstance().MRConfig == GlobalToggle.Configuration.eyesfree && ID == ChalktalkBoard.activeBoardID) {
-                                curve = pool.GetCTEntityText();
-                                curve.InitWithText(textStr, translation, scale, 0/*renderer.facingDirection*/, color, ctType, ID);
-                                curve.isDup = true;
-                                sketchLines.Add(curve);
-                            }
                         }
-                        return;
                     }
-
-                    // otherwise parse into points for stroke or fill
-                    int pointCount = (length - 12) / 4;
-                    Vector3[] points = new Vector3[pointCount];
-                    for (int j = 0; j < pointCount; j += 1) {
-                        Vector3 point = Utility.ParsetoRealVector3(bytes, cursor, 1);
-                        points[j] = point;
-                        cursor += 6 * 2;
-                        width = Utility.ParsetoRealFloat(bytes, cursor);
-                        cursor += 2 * 2;
-                    }
-
-                    // if (!norender) {
-                    switch ((ChalktalkDrawType)type) {
-                    case ChalktalkDrawType.STROKE: {
-                        SketchCurve curve = pool.GetCTEntityLine();
-                        curve.InitWithLines(points, /*isFrame ? new Color(1, 1, 1, 1) : */ color, width * 3, ctType, ID);
-                        sketchLines.Add(curve);
-                        if (GlobalToggleIns.GetInstance().MRConfig == GlobalToggle.Configuration.eyesfree) {
-                            sketchLines[sketchLines.Count - 1].isDup = true;
-                            if (ID == ChalktalkBoard.activeBoardID) {
-                                curve = pool.GetCTEntityLine();
-                                curve.InitWithLines(points, /*isFrame ? new Color(1, 1, 1, 1) : */ color, width * 3, ctType, ID);
-                                sketchLines.Add(curve);
-                            }
-                        }
-                        break;
-                    }
-                    case ChalktalkDrawType.FILL: {
-                        SketchCurve curve = pool.GetCTEntityFill();
-                        curve.InitWithFill(points, /*isFrame ? new Color(1, 1, 1, 1) : */ color, ctType, ID);
-                        sketchLines.Add(curve);
-                        if (GlobalToggleIns.GetInstance().MRConfig == GlobalToggle.Configuration.eyesfree) {
-                            sketchLines[sketchLines.Count - 1].isDup = true;
-                            if (ID == ChalktalkBoard.activeBoardID) {
-                                curve = pool.GetCTEntityLine();
-                                curve.InitWithFill(points, /*isFrame ? new Color(1, 1, 1, 1) : */ color, ctType, ID);
-                                sketchLines.Add(curve);
-                            }
-                        }
-                        break;
-                    }
-                    default: {
-                        break;
-                    }
-                    }
-                    //}                
+                    return;
                 }
+
+                // otherwise parse into points for stroke or fill
+                int pointCount = (length - 12) / 4;
+                Vector3[] points = new Vector3[pointCount];
+                for (int j = 0; j < pointCount; j += 1) {
+                    Vector3 point = Utility.ParsetoRealVector3(bytes, cursor, 1);
+                    points[j] = point;
+                    cursor += 6 * 2;
+                    width = Utility.ParsetoRealFloat(bytes, cursor);
+                    cursor += 2 * 2;
+                }
+
+                // if (!norender) {
+                switch ((ChalktalkDrawType)type) {
+                case ChalktalkDrawType.STROKE: {
+                    SketchCurve curve = pool.GetCTEntityLine();
+                    curve.InitWithLines(points, /*isFrame ? new Color(1, 1, 1, 1) : */ color, width * 3, ctType, ID);
+                    sketchLines.Add(curve);
+                    if (GlobalToggleIns.GetInstance().MRConfig == GlobalToggle.Configuration.eyesfree) {
+                        sketchLines[sketchLines.Count - 1].isDup = true;
+                        if (ID == ChalktalkBoard.activeBoardID) {
+                            curve = pool.GetCTEntityLine();
+                            curve.InitWithLines(points, /*isFrame ? new Color(1, 1, 1, 1) : */ color, width * 3, ctType, ID);
+                            sketchLines.Add(curve);
+                        }
+                    }
+                    break;
+                }
+                case ChalktalkDrawType.FILL: {
+                    SketchCurve curve = pool.GetCTEntityFill();
+                    curve.InitWithFill(points, /*isFrame ? new Color(1, 1, 1, 1) : */ color, ctType, ID);
+                    sketchLines.Add(curve);
+                    if (GlobalToggleIns.GetInstance().MRConfig == GlobalToggle.Configuration.eyesfree) {
+                        sketchLines[sketchLines.Count - 1].isDup = true;
+                        if (ID == ChalktalkBoard.activeBoardID) {
+                            curve = pool.GetCTEntityLine();
+                            curve.InitWithFill(points, /*isFrame ? new Color(1, 1, 1, 1) : */ color, ctType, ID);
+                            sketchLines.Add(curve);
+                        }
+                    }
+                    break;
+                }
+                default: {
+                    break;
+                }
+                }
+                //}                
             }
         }
 
@@ -158,8 +156,9 @@ namespace Chalktalk
             //Debug.Log("CURVE COUNT: " + sketchLineCnt);
             cursor += 2;
 
-
-            ParseEachStroke( bytes, ref cursor, ref sketchLines, ref pool);
+            for (; cursor < bytes.Length;) {
+                ParseEachStroke(bytes, ref cursor, ref sketchLines, ref pool);
+            }
         }
 
     }
