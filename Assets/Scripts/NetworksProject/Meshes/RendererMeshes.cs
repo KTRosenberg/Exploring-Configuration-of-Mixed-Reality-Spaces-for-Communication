94a635a21ab3b80b6ea02d6663df0259835e183d
Zhenyi He <snowymohehe@gmail.com>
2019-03-25 20:33:55 -0400

    :bug: zoffset is correct with eyesfree; and check dominantHand in the scene for right hand

--- Assets/Scripts/NetworksProject/Meshes/RendererMeshes.cs
+++ Assets/Scripts/NetworksProject/Meshes/RendererMeshes.cs
@@ -251,6 +251,7 @@ public class RendererMeshes : MonoBehaviour {
                     meshGO2.Init(meshData);
                     ApplyBoardToMesh(meshGO2);
                     meshGO2.transform.localScale = new Vector3(meshGO2.transform.localScale.x, meshGO2.transform.localScale.y, 0.000001f);
+                    meshGO2.transform.localPosition = new Vector3(meshGO2.transform.localPosition.x, meshGO2.transform.localPosition.y, 0f);
                 }                
             }
 
